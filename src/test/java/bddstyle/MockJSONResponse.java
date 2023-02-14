6fa3983f0b3aa3e477760c372fb7318a8e1c8f88
Gourav <gourav@g.com>
2023-02-14 20:29:27 +0530

    mocking the response

---
+++ src/test/java/bddstyle/MockJSONResponse.java
@@ -0,0 +1,54 @@
+package bddstyle;
+
+import org.testng.annotations.Test;
+
+import io.restassured.path.json.JsonPath;
+import jsonbody.JsonString;
+
+public class MockJSONResponse {
+	
+	@Test(priority = 1)
+	public void getPurchaseAmount()
+	{
+	JsonPath jp = new JsonPath(JsonString.mockjson());
+		
+	int	purchaseamount = jp.getInt("dashboard.purchaseAmount");
+	
+	System.out.println(purchaseamount);//910
+	
+	Object website = jp.get("dashboard.website");
+	
+	System.out.println(website);//www.google.com
+	
+	}
+	
+	@Test(priority = 2)
+	public void getCourseCount()
+	{
+		JsonPath jp = new JsonPath(JsonString.mockjson());
+		
+		int totalnumberofcourse = jp.getInt("courses.size()");
+		
+		System.out.println(totalnumberofcourse);//3
+		
+		
+	}
+	
+	@Test(priority = 3)
+	public void getTitleOfAllCourses()
+	{
+		JsonPath jp = new JsonPath(JsonString.mockjson());
+		
+		int count = jp.getInt("courses.size()");
+		
+//		String titleoffirstcourse= jp.getString("courses[0].title");
+//		System.out.println(titleoffirstcourse);
+		
+		for(int i=0; i<count; i++)
+		{
+			String coursetitle = jp.getString("courses["+i+"].title");
+			System.out.println(coursetitle);
+		}
+	}
+
+}
--- src/test/java/bddstyle/MultiAPIValidation.java
+++ src/test/java/bddstyle/MultiAPIValidation.java
@@ -1,15 +1,19 @@
 package bddstyle;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import jsonbody.JsonString;
 
 import static io.restassured.RestAssured.*;
 
 public class MultiAPIValidation {
+	static String message;
 	
-	@Test
+	
+	@Test(priority = 1)
 	public void createUser()
 	{
 		RestAssured.baseURI = "https://petstore.swagger.io/v2";
@@ -33,12 +37,48 @@ public class MultiAPIValidation {
 	
 	.asPrettyString();
  
+ JsonPath jp = new JsonPath(response);
  
+  message = jp.getString("message");
+ 
+ System.out.println("Message value is "+message);
+ 
+ 	
+	}
 	
 	
+	@Test(priority = 2)
+	public void getUserDetails()
+	{
+		
+		RestAssured.baseURI = "https://petstore.swagger.io/v2";
+		
+		String response = given().header("Content-Type", "application/json")
+		.when()
+		.get("user/userfive")
+		.then()
+		.log().all()
+		.extract()
+		.response()
+		.asPrettyString();
+		
+		
+		JsonPath jp = new JsonPath(response);
+		
+		String idvalue = jp.getString("id");
+		
+		System.out.println("id value is :"+idvalue);
+		
+		
+		Assert.assertEquals(idvalue, message);
+		
+		
 		
 		
 		
 	}
+	
+	
+	
 
 }
